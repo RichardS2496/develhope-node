837d6d44ee6144eb12ea0fbe34cce17102b799e3
Richard Suarez <richardmsc24@gmail.com>
2024-06-02 15:39:19 +0200

    third exercise

---
+++ 03 - Create and use CommonJS modules/exporting.js
@@ -0,0 +1,13 @@
+const mathOperations = {
+  sum: function sum(a, b) {
+    return a + b;
+  },
+  rest: function rest(a, b) {
+    return a - b;
+  },
+  mult: function mult(a, b) {
+    return a * b;
+  },
+};
+
+module.exports = mathOperations;
---
+++ 03 - Create and use CommonJS modules/importing.js
@@ -0,0 +1,5 @@
+const mathOperations = require("./exporting");
+
+console.log(mathOperations.sum(5, 50));
+console.log(mathOperations.rest(5, 23));
+console.log(mathOperations.mult(17, 5));
